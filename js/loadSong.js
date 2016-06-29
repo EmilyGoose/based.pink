47092dd87c42f2de07ac6b62df30900091139874
plethorum <samyar.vahid@protonmail.coom>
2016-06-29 12:41:20 -0400

    bugpls

--- js/loadSong.js
+++ js/loadSong.js
@@ -2,7 +2,7 @@ function loadSong(){
 
     var player=document.getElementById('player');
     var source1=document.getElementById('source1');
-    var source2=document.getElementById('player');
+    var source2=document.getElementById('source2');
 
     source1.src='/audio/mac+.mp3';
     source2.src='/audio/mac-slowed.mp3';
