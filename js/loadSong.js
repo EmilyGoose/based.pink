f82b555111cd7390267d19752b3a70dd16639c92
plethorum <samyar.vahid@protonmail.coom>
2016-06-29 12:06:53 -0400

    Tried troubleshooting JS

--- js/loadSong.js
+++ js/loadSong.js
@@ -1,9 +1,12 @@
 function loadSong(){
-  var player=document.getElementById('player');
-  var source1=document.getElementById('player');
-  var source2=document.getElementById('player');
-  source1.src='/audio/mac+.mp3';
-  source2.src='/audio/mac-slowed.mp3';
-  player.load(); //preloads
-  player.play(); //plays 
+
+    var player=document.getElementById('player');
+    var source1=document.getElementById('player');
+    var source2=document.getElementById('player');
+
+    source1.src='/audio/mac+.mp3';
+    source2.src='/audio/mac-slowed.mp3';
+
+   player.load(); //just start buffering (preload)
+   player.play(); //start playing
 }
