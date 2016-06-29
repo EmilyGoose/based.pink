ce7dafeda47eda21387bb8a42ad92649162eb525
plethorum <samyar.vahid@protonmail.coom>
2016-06-29 10:32:35 -0400

    Created button to switch songs
    Also, created new branch for unstable additions

---
+++ js/loadSong.js
@@ -0,0 +1,9 @@
+function loadSong(){
+  var player=document.getElementById('player');
+  var source1=document.getElementById('player');
+  var source2=document.getElementById('player');
+  source1.src='/audio/mac+.mp3';
+  source2.src='/audio/mac-slowed.mp3';
+  player.load(); //preloads
+  player.play(); //plays 
+}
