e460abe6f967b617137ac92585a921b0dcaef422
plethorum <samyar.vahid@gmail.com>
2016-06-29 21:57:58 -0400

    This *might* work?

--- js/loadSong.js
+++ js/loadSong.js
@@ -3,15 +3,20 @@ var source1 = document.getElementById('source1');
 var source2 = document.getElementById('source2');
 
 player.onclick = function() {
+	var i = 1
 	while (true) {
-    nextTrack = "2";
-    source1.play();
-    source2.pause();
-    source2.currentTime = 0;
-  } else {
-    nextTrack = "1";
-    source2.play();
-    source1.pause();
-    source1.currentTime = 0;
-  }
+		if (i%2 == 0) {
+	    nextTrack = "2";
+	    source1.play();
+	    source2.pause();
+	    source2.currentTime = 0;
+			i++;
+	  } else {
+	    nextTrack = "1";
+	    source2.play();
+	    source1.pause();
+	    source1.currentTime = 0;
+			i++;
+	  }
+	}
 }
