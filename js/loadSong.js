01b9e709f37ca65a48f1fab9f5aef60bd43365a8
plethorum <samyar.vahid@gmail.com>
2016-06-30 13:42:33 -0400

    FINALLY fixed the js for good

--- js/loadSong.js
+++ js/loadSong.js
@@ -1,19 +1,17 @@
 var player = document.getElementById('player');
 var source1 = document.getElementById('source1');
 var source2 = document.getElementById('source2');
-
-player.onclick = function() {
-	var i = 1;
+var i = 1;
+function loadSong(){
 	if (i%2 === 0) {
     source1.play();
     source2.pause();
     source2.currentTime = 0;
 		i++;
-		console.log(i);
   } else {
     source2.play();
     source1.pause();
     source1.currentTime = 0;
 		i++;
 	}
-};
+}
