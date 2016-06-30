56599d957913b800846f32b3d88332df64e5ff93
plethorum <samyar.vahid@gmail.com>
2016-06-29 23:30:04 -0400

    pls to js

--- js/loadSong.js
+++ js/loadSong.js
@@ -4,20 +4,16 @@ var source2 = document.getElementById('source2');
 
 player.onclick = function() {
 	var i = 1
-	while (true) {
-		if (i%2 == 0) {
-	    nextTrack = "2";
-	    source1.play();
-	    source2.pause();
-	    source2.currentTime = 0;
-			i++;
-			console.log(i);
-	  } else {
-	    nextTrack = "1";
-	    source2.play();
-	    source1.pause();
-	    source1.currentTime = 0;
-			i++;
-	  }
+	if (i%2 == 0) {
+    source1.play();
+    source2.pause();
+    source2.currentTime = 0;
+		i++;
+		console.log(i);
+  } else {
+    source2.play();
+    source1.pause();
+    source1.currentTime = 0;
+		i++;
 	}
 }
