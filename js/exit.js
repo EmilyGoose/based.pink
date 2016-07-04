e2abbacfc322e61732a7e8419654261a171771ae
plethorum <samyar.vahid@gmail.com>
2016-07-04 18:57:59 -0400

    added messages

--- js/exit.js
+++ js/exit.js
@@ -4,7 +4,13 @@ message = document.getElementById("message")
 button.onclick = function exit() {
   counter++;
   console.log(counter);
-  if (counter >="3") {
+  if (counter == 3) {
     message.innerHTML = "haha sorry lol";
   }
+	if (counter == 10) {
+		message.innerHTML = "why are you still clicking lol";
+	}
+	if (counter == 15) {
+		message.innerHTML = "Dude stop trying chill out";
+	}
 }
