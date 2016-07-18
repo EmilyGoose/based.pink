d457a4540524659b1d94250ab6244dad7abb7ab0
plethorum <samyar.vahid@gmail.com>
2016-07-18 00:18:46 -0400

    whats the point anyway

--- js/regex.js
+++ js/regex.js
@@ -2,7 +2,7 @@ var clientInfo = navigator.userAgent;
 var re = /(mobile)/i;
 var found = clientInfo.match(re);
 
- window.onload = function mobileOrNot() {
+ function mobileOrNot() {
 	 if (found) {
 	 	document.getElementById("box").style.position=relative;
 		}
