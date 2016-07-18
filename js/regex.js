5a7dfa6a5388254cef8646a8c1187bb6f827bbcf
plethorum <samyar.vahid@gmail.com>
2016-07-18 00:11:56 -0400

    pls microsoft

--- js/regex.js
+++ js/regex.js
@@ -2,7 +2,7 @@ var clientInfo = navigator.userAgent;
 var re = /(mobile)/i;
 var found = clientInfo.match(re);
 
- function mobileOrNot() {
+ window.onload = function mobileOrNot() {
 	 if (found) {
 	 	document.getElementById("box").style.position=relative;
 		else {
