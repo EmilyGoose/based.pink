d7f012341ed90455f8cb0e3570cacaef9c028e8f
plethorum <samyar.vahid@gmail.com>
2016-07-18 00:37:30 -0400

    too much optimiz

--- js/regex.js
+++ js/regex.js
@@ -5,6 +5,8 @@ var found = clientInfo.match(re);
  function mobileOrNot() {
 	 if (found) {
 	 	document.getElementById("box").style.position= "relative";
+		document.getElementById("box").style.margin= "auto";
+
 		}
 		else {
 			document.getElementById("box").style.position= "absolute";
